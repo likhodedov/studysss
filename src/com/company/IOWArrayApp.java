40a883ed71895c10a1316a194c1fcf8803301c25
d.lihodedov <likhodedovdmitry@gmail.com>
2016-07-22 18:07:25 +0600

    update

--- src/com/company/IOWArrayApp.java
+++ src/com/company/IOWArrayApp.java
@@ -4,7 +4,8 @@ package com.company;
  * Created by d.lihodedov on 22.07.2016.
  */
 public class IOWArrayApp {
-
-
+    public static void main(String[] args) {
+        IOWArray arr = new IOWArray(15);
     }
+}
 
