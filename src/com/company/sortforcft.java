0bcf8e129bd24a5997ed2b7d52250ee5e11fe6b8
likho <likhodedovdmitry@gmail.com>
2016-09-14 20:40:37 +0700

    new changes

--- src/com/company/sortforcft.java
+++ src/com/company/sortforcft.java
@@ -4,9 +4,6 @@ import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-/**
- * Created by d.lihodedov on 13.09.2016.
- */
 public class sortforcft {
     public static void main(String[] args)  {
         try {
@@ -15,42 +12,45 @@ public class sortforcft {
         String myJarPath = sortforcft.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         String dirPath = new File(myJarPath).getParent();
         Paths.get(dirPath).toAbsolutePath();
-
-        //System.out.println(dirPath);
         ArrayList<String> dan = new ArrayList<String>();
-       // if (!isDigit(str)) System.out.println("строка");
-       // else System.out.println("число");
-      try(BufferedReader br = new BufferedReader(new FileReader(dirPath+"//"+args[0]))) {
-          //----- ЕСЛИ ЧИСЛО
-          if (args[1].equals("-i")){
-          while((line = br.readLine()) != null)
+            if (((!args[1].equals("-s"))&&(!args[1].equals("-i")))|((!args[2].equals("-a"))&&(!args[2].equals("-d"))))//||(args[1].equals("-i")))||!((args[2]=="-a")||(args[2]=="-d")))
+            {
+                System.out.println("Invalid arguments");
+                return;
+            }
+            if ((args.length>3))
+            {
+                System.out.println("Too much arguments");
+                return;
+            }
+            try(BufferedReader br = new BufferedReader(new FileReader(dirPath+"//"+args[0]))) {
+          //-----считывание, если числа
+          if (args[1].equals("-i"))
           {
+          while((line = br.readLine()) != null)
+            {
               if (line.length()!=0)
               {
-                 //System.out.println(line);
                  if (!isDigit(line))
                     {
-                     System.out.println("Присутствует строка вместо числа. Программа остановлена");
+                     System.out.println("Input value is a string instead of a number. Program has stopped");
                      break;
                     }
                   dan.add(line);
                   count++;
               }
-          }}
-
-
-
-//-----------------------------
+            }
+          }
+//-----------если строки
               else if (args[1].equals("-s"))
                 {
                     while((line = br.readLine()) != null)
-                    {
+                    {line=line.replaceAll("\\s","");
                         if (line.length()!=0)
                         {
-                            //System.out.println(line);
-                            if ((isDigit(line))|(line.matches(".*\\d+.*")))//или добавить что есть цифра в строке
+                            if ((isDigit(line))|(line.matches(".*\\d+.*")))
                             {
-                                System.out.println("Присутствует число в строках или цифры. Программа остановлена");
+                                System.out.println("Input value is a number instead of a string or string contains numeral. Program has stopped");
                                 break;
                             }
                             dan.add(line);
@@ -59,18 +59,15 @@ public class sortforcft {
                     }
                 }
 //----------------------------------------------------------------------
-
           if (count>=100) {
               System.out.println("Too many elements at file");
               return;
           }
-
         } catch (FileNotFoundException e) {
             System.out.println("File not found  ");
         } catch (IOException e) {
-          System.out.println("something wrong with file ");
-        }
-//--------------сортировка строк от a до я
+          System.out.println("Something wrong with input file ");}
+//--------------сортировка строк
        if (args[1].equals("-s")){
             int in,out;
             for (out=1;out<dan.size();out++)
@@ -92,8 +89,6 @@ public class sortforcft {
                 dan.set(in,temp);
             }
         }
-
-
  //-------------Сортировка чисел
             if (args[1].equals("-i")){
                 int in,out;
@@ -118,29 +113,21 @@ public class sortforcft {
             }
 //---------------запись результата в файл
         Writer writer = null;
-        //System.out.println(count);
-        try {
+         try {
             writer = new BufferedWriter(new OutputStreamWriter(
                     new FileOutputStream(dirPath+"\\output.txt"), "utf-8"));
             for (int i=0;i<dan.size();i++) {
                 writer.write(dan.get(i)+System.lineSeparator());
-
-               // Integer.toString(count)
             }
         } catch (IOException ex) {
-
         } finally {
-            try {writer.close();} catch (Exception ex) {/*ignore*/}
+            try {writer.close();} catch (Exception e) {System.out.println("Something wrong with output file"); return;}
         }
 //----------------------
     }
     catch (ArrayIndexOutOfBoundsException e){
-        System.out.println("Не хватает аргументов");
+        System.out.println("Not enough arguments");    }
     }
-    }
-
-
-
     private static boolean isDigit(String s) throws NumberFormatException {
         try {
             Integer.parseInt(s);
